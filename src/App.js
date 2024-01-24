b8cdc6b542e31e0c62ad9677e15a67f843f72998
ljjjjjjjjjjjjjjjjjjjjjj <la.juozaityte@gmail.com>
2024-01-24 10:50:04 +0200

    Added new Promise with fetchData

--- src/App.js
+++ src/App.js
@@ -1,35 +1,67 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import ComparisonTable from './ComparisonTable';
 import ProductCard from './ProductCard';
 
+const busimiDuomenys =  [
+  {
+    name: "Cherry",
+    price: "$1.99",
+    imageUrl: "/images/cherry.png"
+
+  },
+  {
+    name: "Orange",
+    price: "$1.99",
+    imageUrl: "/images/orange.png"
+
+  },
+  {
+    name: "Nuts",
+    price: "$1.99",
+    imageUrl: "/images/nuts.png"
+
+  },
+  {
+    name: "Strawberry",
+    price: "$1.99",
+    imageUrl: "/images/strawberry.png"
+
+  }
+];
+
+const fetchData = () => {
+  return new Promise( (resolve, reject) => {
+  
+    setTimeout( () => {
+      const success = Math.random() > 0.3; // 70% tikimybe
+      if(success){
+        resolve ( busimiDuomenys );
+      } else {
+        reject ("Duomenų užkrauti nepavyko");
+      }
+
+    }, 1000);
+
+  } );
+
+}
+
 function App() {
-  const products = [
-    {
-      name: "Cherry",
-      price: "$1.99",
-      imageUrl: "/images/cherry.png"
+  const [products, setProducts] = useState( [] );
 
-    },
-    {
-      name: "Orange",
-      price: "$1.99",
-      imageUrl: "/images/orange.png"
+  useEffect( () => {
 
-    },
-    {
-      name: "Nuts",
-      price: "$1.99",
-      imageUrl: "/images/nuts.png"
+    fetchData()
+    .then( d => setProducts( d ) )
+    .catch( e => console.error (e))
 
-    },
-    {
-      name: "Strawberry",
-      price: "$1.99",
-      imageUrl: "/images/strawberry.png"
+    console.log("Promisas paleistas");
+  
+  } , [] );
 
-    }
-  ];
 
+   
   return (
     <div className="app">
       <h1>Compare Products</h1>
